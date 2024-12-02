6a33aaaf9e997ef722286a1621edd533d48e54ec
BuyCommoditys <tech@buycommodity.in>
2024-12-02 20:19:21 +0530

    Update config.js

--- buycom/config.js
+++ buycom/config.js
@@ -1,4 +1,4 @@
 // src/config.js
-const API_URL = 'http://52.66.140.249/api/'; // Replace with your backend API URL
+const API_URL = 'http://52.66.140.249/api'; // Replace with your backend API URL
 
 export default API_URL;
