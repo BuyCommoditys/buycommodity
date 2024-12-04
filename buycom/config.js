92e5e8058264781c738d2f8bee7d83723c7ff58b
BuyCommoditys <tech@buycommodity.in>
2024-12-04 12:26:31 +0530

    Update config.js

--- buycom/config.js
+++ buycom/config.js
@@ -1,4 +1,4 @@
 // src/config.js
-const API_URL = 'https://buycom.onrender.com/api'; // Replace with your backend API URL
+const API_URL = 'https://tech.buycommodity.in/api'; // Replace with your backend API URL
 
 export default API_URL;
